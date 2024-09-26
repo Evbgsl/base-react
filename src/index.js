7505acc2eb674539d1172cd5475d36b196baa94d
Evbgsl <evbgsl@gmail.com>
2024-09-26 11:53:31 +0300

    Code refactored

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { App } from './App';
 const $root = document.querySelector('#root');
 const root = createRoot($root);
 
-root.render(<App name="React component examples"/>);
+root.render(<App name="React component exmpls"/>);
