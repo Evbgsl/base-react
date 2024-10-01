866d42b4d8163681364abd74aed228575301688a
Evbgsl <evbgsl@gmail.com>
2024-10-01 06:38:55 +0300

    WIP

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './app';
-
-const $root = document.querySelector('#root');
-if ($root) {
-  const root = createRoot($root);
-  root.render(<App name={'React component xmpls'}/>);
-}
